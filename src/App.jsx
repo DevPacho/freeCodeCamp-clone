aaf9f5ec37fc986d3631b04aa2a07df392c5a1e4
DevPacho <frankjose2004@gmail.com>
2022-09-29 00:13:58 -0500

    Testing deployment changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="main-container">
-        <h1>Here is what our alumni say about freeCodeCamp:</h1>
+        <h1>Here is what our alumni say about freeCodeCamp: Pacho</h1>
 
         <Testimony
           name="Shawn Wang"
